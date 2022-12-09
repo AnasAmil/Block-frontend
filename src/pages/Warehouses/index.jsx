ed1c9407830656f9b104bb724ba838ebac35d99e
Anas Amaneellah <anas@Anass-MacBook-Air.local>
2022-12-09 01:51:06 +0100

    dynamic warehouses grid and routing

--- src/pages/Warehouses/index.jsx
+++ src/pages/Warehouses/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { colorCodes } from '../../theme'
-import { Box, useTheme, Grid, Card } from '@mui/material'
+import { Box, useTheme, Grid, Card, Typography, Button } from '@mui/material'
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import { mockDataWarehouses } from '../../data/MockData'
 import Header from '../../components/Header'
-import LogoOnly from '../../assets/block-logo-only.png'
+import WarehouseImg from '../../assets/Warehouse-img.jpg'
+import { Link } from 'react-router-dom';
 
 const Warhouses = () => {
 
@@ -15,26 +20,76 @@ const Warhouses = () => {
           <Header title='Warehouses' subtitle='This is your warehouses' />
         </Box>
 
-        <Box>
-            <Grid
-              sx={{ flexGrow: 1 }}
-              container
-              spacing={2}
-            >
+        <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+            <Grid item xs={12}>
+                <Grid container justifyContent="center" spacing={10}>
+                  {mockDataWarehouses.map((warehouse, index) => (
+                    <Grid key={index} item>
+                      <Card
+                        sx={{
+                          height: 380,
+                          width: 300,
+                          backgroundColor: colors.primary[400]
+                        }}
+                      >
 
-              <Grid item xs={12} >
-                <Card
-                  sx={{
-                    maxWidth: 275,
-                    backgroudColor: colors.primary[400]
-                  }}
-                >
-                  test
-                </Card>
-              </Grid>
+                        <CardMedia 
+                          component='img'
+                          image={WarehouseImg}
+                        />
 
+                        <CardContent
+                          sx={{
+                            display: 'flex',
+                            flexDirection : 'column',
+                            justifyContent: 'space-between'
+                          }}
+                        >
+                            <Typography
+                              variant='h4'
+                              sx={{
+                                fontWeight: 'bold',
+                                color: colors.greenVibrant[500]
+                              }}
+                            >
+                              {warehouse.name}
+                            </Typography>
+
+                            <Typography
+                              variant='h6'
+                            >
+                              <span style={{ fontWeight: 'bold' }}>Location: </span>{warehouse.location}
+                            </Typography>
+
+                            <Typography
+                              variant='h6'
+                            >
+                              <span style={{ fontWeight: 'bold' }}>Cells: </span>{warehouse.cells}
+                            </Typography>
+                        </CardContent>
+
+                        <CardActions>
+                          <Button 
+                            variant='text'
+                            component={Link}
+                            sx={{
+                              color: colors.orangeVibrant[500],
+                              fontWeight: 'bold'
+                            }}
+                            to={`/warehouses/${warehouse.id}`}
+                          >
+                            See Warehouse
+                          </Button>
+                        </CardActions>
+
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
             </Grid>
-        </Box>
+      
+        </Grid>
+        
     </>
   )
 }
